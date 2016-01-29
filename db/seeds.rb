66f8ad69dfa193799ca3df1cbc7d48a67204ce96
rjjq <rrrjjjqqq@gmail.com>
2016-01-29 22:41:25 +0800

    加入seed & 自動化重整資料庫程式

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "hi"
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用'])
+
+create_groups = 1.upto(20) do |i|
+  Group.create!([title: "Group no. #{i}", description: "用seed建立的第#{i}個討論版", user_id: "7"])
+  1.upto(30) do |k|
+    Post.create!([group_id: "#{i}", content: "用seed建立的第#{k}個留言", user_id: "7"])
+  end
+end
